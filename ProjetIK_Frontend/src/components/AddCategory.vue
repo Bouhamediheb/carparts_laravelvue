1f42d1a23f3a2c3cca0cf764cf1a337f95f452d4
ihebbouhamed <ihebbouhamediit@gmail.com>
2024-01-12 11:43:40 +0100

    Add Category Form

---
+++ ProjetIK_Frontend/src/components/AddCategory.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class ="add-category-page">
+        <form @submit.prevent="submit" class="card">
+          <h2>Add Category</h2>
+
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" id="name" v-model="form.name" class="form-control" required>
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+.add-category-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.card {
+  width: 400px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  text-align: center;
+}
+
+.form-field {
+  margin-bottom: 20px;
+}
+</style>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            form: {
+                name: '',  
+            },
+        };
+    },
+    methods: {
+        submit() {
+            axios.post('http://localhost:8000/api/categories', this.form)
+                .then(() => {
+                    this.$router.push({ name: 'Categories' });
+                });
+        },
+    },
+    
+};
+</script>
--- ProjetIK_Frontend/src/router/router.js
+++ ProjetIK_Frontend/src/router/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AddProduct from '../components/AddProduct.vue';
 import Register from '../components/Views/User/Register.vue';
 import Login from '../components/Views/User/Login.vue';
+import AddCategory from '../components/AddCategory.vue';
 
 const routes = [
   { path: '/addproduct', component: AddProduct },
   { path: '/register', component : Register },
   { path: '/login', component : Login },
+  {path :'/addcategory', component : AddCategory},
   
 ];
 
