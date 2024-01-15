4917757f7ad81cc577f2ba8a50caba5db0e1f958
Karim Hachicha <hachicha.karim@iit.ens.tn>
2024-01-15 12:02:42 +0100

    Home Page

--- ProjetIK_Frontend/src/App.vue
+++ ProjetIK_Frontend/src/App.vue
@@ -2,9 +2,11 @@
   <div>
     <Header></Header>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
 
 <script setup>
+import Footer from './components/Footer.vue';
 import Header from './components/Header.vue';
 </script>
---
+++ ProjetIK_Frontend/src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+    <div style = "background-color: rgba(246, 246, 246, 1);">
+        <div class = "row text-center">
+            <div class="col-sm mt-3">
+            <Avatar icon="pi pi-car" class="mr-2" size="xlarge" shape="circle" style = "background-color: white; color : #4CAF50" />
+            <div>
+                <h6>Free Shipping</h6>
+                <p>On all orders over 99 TND</p>
+            </div>
+            </div>
+            <div class="col-sm mt-3">
+            <Avatar icon="pi pi-sync" class="mr-2" size="xlarge" shape="circle" style = "background-color: white; color : #4CAF50" />
+            <div>
+                <h6>30 Days Return</h6>
+                <p>You have 30 days to return</p>
+            </div>
+            </div>
+            <div class="col-sm mt-3">
+            <Avatar icon="pi pi-lock" class="mr-2" size="xlarge" shape="circle" style = "background-color: white; color : #4CAF50" />
+            <div>
+                <h6>Safe Shopping</h6>
+                <p>Payment 100% secure</p>
+            </div>
+            </div>
+            <div class="col-sm mt-3">
+            <Avatar icon="pi pi-users" class="mr-2" size="xlarge" shape="circle" style = "background-color: white; color : #4CAF50" />
+            <div>
+                <h6>Online Support</h6>
+                <p>Contact Us 24 hours a day</p>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class = "row text-center">
+        <div class = "col-sm d-flex">
+            
+        </div>
+        
+        
+    </div>
+</div>
+</template>
+
+<script setup>
+import Avatar from 'primevue/avatar';
+import Menubar from 'primevue/menubar';
+
+</script>
